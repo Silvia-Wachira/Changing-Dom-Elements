905748bc017848e57befa578dc9609c094639ae9
Silvia-Wachira <wachirasilvia8@gmail.com>
2023-06-29 15:24:57 +0300

    add an image

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 function updateWeekDay(weekday){
     let h4 = document.querySelector('h4')
-    h4.textContent = weekday
-    h4.id = weekday.toLowerCase()
-    h4.className  = 'title'
-    h4.style.color = 'green'
-    h4.style.fontSize = '30px'
+    // h4.textContent = weekday
+    // h4.id = weekday.toLowerCase()
+    // h4.className  = 'title'
+    // h4.style.color = 'green'
+    // h4.style.fontSize = '30px'
 }
 updateWeekDay('Monday')
+
+function addImage(img_url){
+    let img = document.querySelector('img')
+    img.src = img_url
+}
+addImage('https://images.pexels.com/photos/850359/pexels-photo-850359.jpeg?auto=compress&cs=tinysrgb&w=400')
