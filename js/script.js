dca305fd414b9746c25aa67ea24293b349e68945
Silvia-Wachira <wachirasilvia8@gmail.com>
2023-06-29 15:43:38 +0300

    create an inner HTML

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 function updateWeekDay(weekday){
     let h4 = document.querySelector('h4')
+    h4.innerHTML = ''
+  
     // h4.textContent = weekday
     // h4.id = weekday.toLowerCase()
     // h4.className  = 'title'
     // h4.style.color = 'green'
     // h4.style.fontSize = '30px'
 }
-updateWeekDay('Monday')
+updateWeekDay('Friday')
 
 function addImage(img_url){
     let img = document.querySelector('img')
     img.src = img_url
 }
-addImage('https://images.pexels.com/photos/850359/pexels-photo-850359.jpeg?auto=compress&cs=tinysrgb&w=400')
+addImage('https://images.pexels.com/photos/796620/pexels-photo-796620.jpeg?auto=compress&cs=tinysrgb&w=400')
